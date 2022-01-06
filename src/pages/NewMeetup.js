8eea52cf8c6be75351a4cf9ed495036d35bfd54b
rajnis0608 <rajnis0608@gmail.com>
2022-01-06 13:20:50 +0530

    Added form to add new items and storing to firebase database

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupsPage(){
-    return <div>New Meetup Page</div>
+
+    const navigate = useNavigate();
+    const nev = event => navigate('/', { replace: true });
+
+    
+
+    function onAddMeetupHandler(meetupData){
+        fetch('https://react-meetups-bcf6f-default-rtdb.firebaseio.com/meetups.json',
+        {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type':'application/json'
+            }
+        }).then(() => {
+            nev();
+        });
+    }
+
+    return <section>
+        <h1>Add New Meetup</h1>
+        <NewMeetupForm onAddMeetup = {onAddMeetupHandler}/>
+    </section>;
 }
 
 export default NewMeetupsPage;
